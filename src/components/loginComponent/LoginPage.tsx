f18acdee265878a1bdc176f747d9a3555fe97d1f
ofirkor <ofirkor1999@gmail.com>
2024-05-05 23:23:44 +0300

    added based routing, fixed dependencies versions and wip login page

--- src/components/loginComponent/LoginPage.tsx
+++ src/components/loginComponent/LoginPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   MDBBtn,
   MDBContainer,
@@ -6,11 +7,15 @@ import {
   MDBInput,
 } from "mdb-react-ui-kit";
 import logo from "../../images/logo.png";
-import axios from 'axios';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   return (
-    <MDBContainer className="my-5 gradient-form" style={{backgroundColor: "#D0B8A8"}}>
+    <MDBContainer
+      className="my-5 gradient-form"
+      style={{ backgroundColor: "#D0B8A8" }}
+    >
       <MDBRow>
         <MDBCol col="6" className="mb-5">
           <div className="d-flex flex-column ms-5">
@@ -21,19 +26,20 @@ function App() {
             <p>Please login to your account</p>
 
             <div className="mb-4">
-              <label htmlFor="form1">
-                Email address
-              </label>
+              <label htmlFor="form1">Email address</label>
               <MDBInput id="form1" type="email" />
             </div>
             <div className="mb-4">
-              <label htmlFor="form2">
-                Password
-              </label>
+              <label htmlFor="form2">Password</label>
               <MDBInput id="form2" type="password" />
             </div>
             <div className="text-center pt-1 mb-5 pb-1">
-              <MDBBtn className="mb-4 w-100 gradient-custom-2" onClick={handleLoginButtonClick}>Login</MDBBtn>
+              <MDBBtn
+                className="mb-4 w-100 gradient-custom-2"
+                onClick={handleLoginButtonClick}
+              >
+                Login
+              </MDBBtn>
               <a className="text-muted" href="#!">
                 Forgot password?
               </a>
@@ -53,7 +59,14 @@ function App() {
             <div className="text-white px-3 py-4 p-md-5 mx-md-4">
               <h4 className="mb-4">Home Harmony</h4>
               <p className="medium mb-0">
-              Home Harmony is a solution designed to make life easier for people sharing living spaces. There's a growing need for a tool that can help with communication, organization, and building a sense of community. Home Harmony does just that. It's a platform that not only helps with day-to-day tasks but also aims to create a strong bond among roommates. Our goal is to go beyond just managing chores and expenses, we want to make shared living enjoyable and organized for everyone involved.
+                Home Harmony is a solution designed to make life easier for
+                people sharing living spaces. There's a growing need for a tool
+                that can help with communication, organization, and building a
+                sense of community. Home Harmony does just that. It's a platform
+                that not only helps with day-to-day tasks but also aims to
+                create a strong bond among roommates. Our goal is to go beyond
+                just managing chores and expenses, we want to make shared living
+                enjoyable and organized for everyone involved.
               </p>
             </div>
           </div>
@@ -63,17 +76,25 @@ function App() {
   );
 }
 
-
-
 const handleLoginButtonClick = async () => {
   try {
-      const email = (document.getElementById('form1') as HTMLInputElement)?.value; 
-      const password = (document.getElementById('form2') as HTMLInputElement)?.value; 
-      const response = await axios.post('/api/login', { email: email , password: password });
-      console.log(response.data);
+    //const navigate = useNavigate();
+    const email = (document.getElementById("form1") as HTMLInputElement)?.value;
+    const password = (document.getElementById("form2") as HTMLInputElement)
+      ?.value;
+    const response = await axios.post("/api/login", {
+      email: email,
+      password: password,
+    });
+    console.log(response.data);
+    if (response.data.success) {
+      console.log("Login successful");
+      //navigate("/home");
+    } else {
+    }
   } catch (error: any) {
-      console.error('Login failed:', error.response.data);
-      // Add logic to handle failed login (e.g., display error message)
+    console.error("Login failed:", error.response.data);
+    // Add logic to handle failed login (e.g., display error message)
   }
 };
 export default App;
