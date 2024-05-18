dee8247b46007964d41f3b87b1e5834139d6f45f
ofirkor <ofirkor1999@gmail.com>
2024-05-18 22:42:05 +0300

    added expenses page, fixed reload login problem and added session continuation on /

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
@@ -9,12 +9,30 @@ import ExpensesPage from "./components/expensesComponent/ExpensesPage";
 import ConfirmPage from "./components/signUpComponent/ConfirmPage";
 
 
+function AppWrapper({userId, setUserId} : {userId: number, setUserId: React.Dispatch<React.SetStateAction<number>>}) {
+  if (userId === -1) {
+    return <LoginPage setUserId={setUserId}/>
+  }
+
+  return <HomePage />;
+}
+
+
 function App() {
   const [userId, setUserId] = useState(-1);
+  
+  useEffect(() => {
+    const savedUserId = localStorage.getItem("userId");
+    if (savedUserId) {
+      console.log("retrieved previous userId: ", savedUserId);
+      setUserId(Number(savedUserId));
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage setUserId={setUserId}/>}/>
+        <Route path="/" element={<AppWrapper userId={userId} setUserId={setUserId}/>}/>
         <Route path="/login" element={<LoginPage setUserId={setUserId}/>} />
         <Route path="/signup" element={<SignUp setUserId={setUserId}/>} />
         <Route path="/home" element={<HomePage />} />
