59aae0f576746fba2572eced5d3adf4cc1520616
moranpol <moranpol3@gmail.com>
2024-05-31 12:21:00 +0300

    fixed bug in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/loginComponent/LoginPage";
@@ -9,27 +9,23 @@ import ConfirmPage from "./components/signUpComponent/ConfirmPage";
 import DocumentsPage from "./components/documentsComponent/DocumentsPage";
 import ApartmentsPage from "./components/apartmentsComponent/ApartmentsPage";
 
-<<<<<<< HEAD
-function AppWrapper({userId, setUserId} : {userId: number, setUserId: React.Dispatch<React.SetStateAction<number>>}) {
-  if (userId === -1) {
-    return <LoginPage setUserId={setUserId}/>
-  }
-=======
-
->>>>>>> 57ad7f46bb263b6965ffdd1ab65fb7c5f69b2103
-
-
 function App() {
   const [userId, setUserId] = useState(-1);
 
-  function AppWrapper({userId, setUserId} : {userId: number, setUserId: React.Dispatch<React.SetStateAction<number>>}) {
+  function AppWrapper({
+    userId,
+    setUserId,
+  }: {
+    userId: number;
+    setUserId: React.Dispatch<React.SetStateAction<number>>;
+  }) {
     if (userId === -1) {
-      return <LoginPage setUserId={setUserId}/>
+      return <LoginPage setUserId={setUserId} />;
     }
-  
-    return <HomePage userId={userId}/>;
+
+    return <HomePage userId={userId} />;
   }
-  
+
   useEffect(() => {
     const savedUserId = localStorage.getItem("userId");
     if (savedUserId) {
@@ -41,14 +37,23 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AppWrapper userId={userId} setUserId={setUserId}/>}/>
-        <Route path="/login" element={<LoginPage setUserId={setUserId}/>} />
-        <Route path="/signup" element={<SignUp setUserId={setUserId}/>} />
-        <Route path="/home" element={<HomePage userId={userId}/>} />
+        <Route
+          path="/"
+          element={<AppWrapper userId={userId} setUserId={setUserId} />}
+        />
+        <Route path="/login" element={<LoginPage setUserId={setUserId} />} />
+        <Route path="/signup" element={<SignUp setUserId={setUserId} />} />
+        <Route path="/home" element={<HomePage userId={userId} />} />
         <Route path="/expenses" element={<ExpensesPage userId={userId} />} />
-        <Route path="/signup/confirm" element={<ConfirmPage userId={userId}/>} />
-        <Route path="/documents" element={<DocumentsPage userId={userId}/>} />
-        <Route path="/join-apartment" element={<ApartmentsPage userId={userId}/>} />
+        <Route
+          path="/signup/confirm"
+          element={<ConfirmPage userId={userId} />}
+        />
+        <Route path="/documents" element={<DocumentsPage userId={userId} />} />
+        <Route
+          path="/join-apartment"
+          element={<ApartmentsPage userId={userId} />}
+        />
       </Routes>
     </BrowserRouter>
   );
