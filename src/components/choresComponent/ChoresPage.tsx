574da374a23a8927b34d477a9920e7ed944e9998
Moran Pol <104639084+moranpol@users.noreply.github.com>
2024-06-08 10:42:01 +0300

    fixed problem that chorespage and mychorestable was empty (#23)

---
+++ src/components/choresComponent/ChoresPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChoresPage = () => {
+    return (
+        <div>
+        <h1>Chores</h1>
+        </div>
+    );
+};
+
+export default ChoresPage;
---
+++ src/components/choresComponent/MyChoresTable.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const MyChoressTable = () => {
+}
+
+export default MyChoressTable;
