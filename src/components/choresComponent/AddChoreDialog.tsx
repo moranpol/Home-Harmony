afc446843719fcc61c2a6f5e6c75a4bfc88b3499
moranpol <moranpol3@gmail.com>
2024-09-05 14:27:32 +0300

    fixed chores page style

--- src/components/choresComponent/AddChoreDialog.tsx
+++ src/components/choresComponent/AddChoreDialog.tsx
@@ -11,6 +11,8 @@ import {
   InputLabel,
   Select,
   SelectChangeEvent,
+  Grid,
+  Typography,
 } from "@mui/material";
 import axios from "axios";
 
@@ -30,8 +32,14 @@ function AddChoreDialog({ open, onClose, userId }: AddChoreDialogProps) {
   const [description, setDescription] = useState("");
   const [isRepeat, setIsRepeat] = useState(false);
   const [repeat, setRepeat] = useState("none");
-  const [selectedRoommate, setSelectedRoommate] = useState<number | null>(null);
-  const [roommates, setRoommates] = useState<{ id: number; fname: string; lname: string }[]>([]);
+  const [selectedRoommate, setSelectedRoommate] = useState<string>("");
+  const [roommates, setRoommates] = useState<
+    { id: number; fname: string; lname: string }[]
+  >([]);
+  const [errors, setErrors] = useState({
+    description: "",
+    selectedRoommate: "",
+  });
 
   useEffect(() => {
     const fetchRoommates = async () => {
@@ -48,7 +56,9 @@ function AddChoreDialog({ open, onClose, userId }: AddChoreDialogProps) {
     }
   }, [open, userId]);
 
-  const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setDescription(event.target.value);
   };
 
@@ -58,78 +68,205 @@ function AddChoreDialog({ open, onClose, userId }: AddChoreDialogProps) {
   };
 
   const handleRoommateChange = (event: SelectChangeEvent) => {
-    setSelectedRoommate(Number(event.target.value));
+    setSelectedRoommate(event.target.value);
+  };
+
+  const validateForm = (): boolean => {
+    let isValid: boolean = true;
+    const errorsObj: {
+      description: string;
+      selectedRoommate: string;
+    } = {
+      description: "",
+      selectedRoommate: "",
+    };
+    if (!description) {
+      errorsObj.description = "Description is required.";
+      isValid = false;
+    }
+    if (!selectedRoommate) {
+      errorsObj.selectedRoommate = "You must select a roommate";
+      isValid = false;
+    }
+
+    setErrors(errorsObj);
+    return isValid;
   };
 
   const handleSubmit = async () => {
-    try {
-      if (!selectedRoommate) {
-        alert("Please select a roommate.");
-        return;
-      }
+    if (validateForm()) {
+      try {
+        if (!selectedRoommate) {
+          alert("Please select a roommate.");
+          return;
+        }
 
-      const newChore = {
-        userId: selectedRoommate,
-        description,
-        isRepeat,
-        repeat,
-      };
+        const newChore = {
+          userId: selectedRoommate,
+          description,
+          isRepeat,
+          repeat,
+        };
 
-      const response = await axios.post("/chores", newChore);
-      if (response.data.success) {
-        alert("Chore added successfully.");
-        onClose(true); // Pass true to indicate the chore was successfully created
-      } else {
-        alert("Failed to add chore.");
+        const response = await axios.post("/chores", newChore);
+        if (response.data.success) {
+          alert("Chore added successfully.");
+          onClose(true);
+        } else {
+          alert("Failed to add chore.");
+        }
+      } catch (error: any) {
+        console.error("Failed to add chore:", error.message);
+        alert("Failed to add chore, please try again.");
       }
-    } catch (error: any) {
-      console.error("Failed to add chore:", error.message);
-      alert("Failed to add chore, please try again.");
     }
   };
 
   const handleClose = () => {
-    onClose(false); // Pass false to indicate no new chore was created
+    onClose(false);
   };
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Add New Chore</DialogTitle>
-      <DialogContent>
-        <TextField
-          margin="dense"
-          label="Description"
-          fullWidth
-          value={description}
-          onChange={handleDescriptionChange}
-          required
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <DialogTitle
+        style={{
+          textAlign: "center",
+          color: "#333333",
+          fontSize: "2rem",
+          letterSpacing: "0.125rem",
+          textShadow: "0.125rem 0.125rem 0.25rem rgba(0, 0, 0, 0.2)",
+          position: "relative",
+        }}
+      >
+        Add New Chore
+        <span
+          style={{
+            content: "",
+            position: "absolute",
+            bottom: "-0.1rem",
+            left: "50%",
+            transform: "translateX(-50%)",
+            width: "5rem",
+            height: "0.1875rem",
+            backgroundColor: "#C3A6A0",
+            borderRadius: "0.125rem",
+          }}
         />
-        <FormControl fullWidth margin="dense">
-          <InputLabel>Assign to</InputLabel>
-          <Select value={selectedRoommate?.toString() || ""} onChange={handleRoommateChange} label="Assign to">
-            {roommates.map((roommate) => (
-              <MenuItem key={roommate.id} value={roommate.id}>
-                {roommate.fname} {roommate.lname}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl fullWidth margin="dense">
-          <InputLabel>Repeat</InputLabel>
-          <Select value={repeat} onChange={handleRepeatChange} label="Repeat">
-            {repeatOptions.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      </DialogTitle>
+      <DialogContent
+        style={{
+          width: "100%",
+          maxWidth: "30rem",
+          padding: "1.3rem",
+          boxSizing: "border-box",
+          overflow: "visible",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              value={description}
+              onChange={handleDescriptionChange}
+              error={Boolean(errors.description)}
+              helperText={errors.description}
+              style={{ width: "100%" }}
+              autoComplete="description"
+              name="description"
+              required
+              fullWidth
+              multiline
+              id="description"
+              label="Description"
+              InputLabelProps={{ shrink: true }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Assign to</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedRoommate}
+                onChange={handleRoommateChange}
+                label="Assign to"
+              >
+                {roommates.map((roommate) => (
+                  <MenuItem key={roommate.id} value={roommate.id.toString()}>
+                    {roommate.fname} {roommate.lname}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+          {errors.selectedRoommate && (
+            <Typography
+              variant="body1"
+              style={{
+                color: "red",
+                textAlign: "left",
+                marginTop: "1rem",
+                marginLeft: "2rem",
+              }}
+            >
+              {errors.selectedRoommate}
+            </Typography>
+          )}
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Repeat</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={repeat}
+                onChange={handleRepeatChange}
+                label="Repeat"
+              >
+                {repeatOptions.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color="primary">
+        <Button
+          onClick={handleClose}
+          style={{
+            textTransform: "none",
+            border: "none",
+            borderRadius: "0.3125rem",
+            cursor: "pointer",
+            fontWeight: "500",
+            transition: "background-color 0.3s ease",
+            color: "#C3A6A0",
+          }}
+        >
           Cancel
         </Button>
-        <Button onClick={handleSubmit} color="primary" variant="contained">
+        <Button
+          onClick={handleSubmit}
+          style={{
+            textTransform: "none",
+            border: "none",
+            borderRadius: "0.3125rem",
+            cursor: "pointer",
+            fontWeight: "500",
+            transition: "background-color 0.3s ease",
+            color: "#C3A6A0",
+          }}
+        >
           Add Chore
         </Button>
       </DialogActions>
